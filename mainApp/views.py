e46c9e10cba4b4048bc9a6968e45a5fbaed8bf54
Infinity10K <33337801+Infinity10K@users.noreply.github.com>
2020-07-15 18:23:13 +0700

    Update views.py

--- mainApp/views.py
+++ mainApp/views.py
@@ -5,6 +5,9 @@ import pandas as pd
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.models import load_model
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import stopwords
+import re
 
 df = pd.read_csv('./models/imdb_clean.zip')
 
@@ -17,14 +20,27 @@ maxlen = 370
 model = load_model('./models/sentem_model.h5')
 model2 = load_model('./models/mark_model.h5')
 
+stop_words = set(stopwords.words("english")) 
+lemmatizer = WordNetLemmatizer()
+
+
+def clean_text(text):
+    text = re.sub(r'[^\w\s]','',text, re.UNICODE)
+    text = text.lower()
+    text = [lemmatizer.lemmatize(token) for token in text.split(" ")]
+    text = [lemmatizer.lemmatize(token, "v") for token in text]
+    text = [word for word in text if not word in stop_words]
+    text = " ".join(text)
+    return text
+
+
 def home(request):
     return render(request, 'index.html')
 
 
 def predict(request):
-    temp = []
-    temp.append(request.POST.get('text'))
-
+    temp = [request.POST.get('text')]
+    temp = [clean_text(temp[0])]
     list_sentences_test = temp
     list_tokenized_test = tokenizer.texts_to_sequences(list_sentences_test)
     X_te = pad_sequences(list_tokenized_test, maxlen=maxlen)
